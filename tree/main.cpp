1677f73a7332768786c886534f7802177f6632e9
Nikita Galushko <galnikrom@gmail.com>
2013-10-12 16:46:34 +0400

    Initial Commit

---
+++ tree/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  tree
+//
+//  Created by Nikita Galushko on 12.10.13.
+//  Copyright (c) 2013 Nikita Galushko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
