684aa0b0abe16f0040897af904ca0b3bf28571aa
Nikita Galushko <galnikrom@gmail.com>
2013-10-12 18:01:19 +0400

    implementation two methods
    find node & insert node

--- tree/Tree.cpp
+++ tree/Tree.cpp
@@ -13,3 +13,57 @@ Tree::Tree(int key, double data, Node* left, Node* right) {
     Node(key, data, left, right);
 }
 
+Node* Tree::find(int key) {
+    
+    Node* current = this->root;
+    
+    while (current->getKey() != key) {
+        
+        if (key < current->getKey()) {
+            current = current->getLeft();
+        }
+        else {
+            current = current->getRight();
+        }
+        
+        if (current == 0)
+            return 0;
+    }
+    
+    return current;
+    
+}
+
+void Tree::insert(int key, double data) {
+    
+    Node *newNode = new Node(key, data);
+    
+    if (this->root == 0)
+        root = newNode;
+    else {
+        Node* current = this->root;
+        Node* parent;
+        
+        while (true) {
+            parent = current;
+            
+            if (key < current->getKey()) {
+                
+                current = current->getLeft();
+                if (current == 0) {
+                    parent->setLeft(newNode);
+                    return;
+                }
+                
+            }
+            else {
+                current = current->getRight();
+                if (current == 0) {
+                    parent->setRight(newNode);
+                    return;
+                }
+            }
+        }
+    }
+    
+}
