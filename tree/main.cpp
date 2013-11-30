4c0705a65ddb5333056b2af7ee0da24dcb7dd1c1
jetuuuu <galnikrom@gmail.com>
2013-11-30 13:09:49 +0400

    test RBTree

--- tree/main.cpp
+++ tree/main.cpp
@@ -20,21 +20,23 @@ int main(int argc, const char * argv[])
     RBTree<double>* theTree = new RBTree<double>(50, 1.5);
     
     theTree->insert(25, 1.7);
-    theTree->insert(15, 2.0);
-    theTree->insert(46, 4.0);
-    theTree->insert(60, 6.0);
-    theTree->insert(54, 5.4);
+    theTree->insert(75, 2.0);
+    theTree->insert(12, 4.0);
+    theTree->insert(18, 6.0);
+    /*theTree->insert(54, 5.4);
     theTree->insert(71, 7.0);
-    theTree->insert(1, 0.0);
+    theTree->insert(1, 0.0);*/
     
     /*RBNode<double>* node = theTree->find(25);
     std::cout<< "Left adress: "<< node->getData()<< std::endl;*/
     
     theTree->inorderTreeWalk(theTree->getRoot());
- 
+    
+    RBNode<double>* node = theTree->getRoot();
+    
     //RBNode<double>* min = theTree->max(theTree->getRoot());
     
-    //std::cout<< std::endl<<"Key: "<<min->getKey()<< std::endl;
+    std::cout<< std::endl<<"Key: "<<node->getKey()<< std::endl;
     
     return 0;
     
